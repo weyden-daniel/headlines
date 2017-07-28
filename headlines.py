e361e64a394533b4fb9e0a0f6a7c7f7c4420c813
Weyden Daniel <weyden.daniel@gmail.com>
2017-07-28 12:17:34 -0300

    little fix

--- headlines.py
+++ headlines.py
@@ -1,14 +1,13 @@
-
 import feedparser
 from flask import Flask
 from flask import render_template
+
 
 app = Flask(__name__)
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
-             'fox': 'http://feeds.foxnews.com/foxnews/latest',
-             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+             'fox': 'http://feeds.foxnews.com/foxnews/latest'}
 
 
 @app.route("/")
